bbfa8745a75691c74248d1128a80ea4dd4775746
whale92130 <alexander92130@gmail.com>
2025-01-11 17:57:18 -0800

    Goodbye

--- src/_03_tea_maker/Tea_Sipper.java
+++ src/_03_tea_maker/Tea_Sipper.java
@@ -1,11 +1,16 @@
 package _03_tea_maker;
 
+import _03_tea_maker.Kettle.Water;
+
 public class Tea_Sipper {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TeaBag passion=new TeaBag("Passion fruit");
-		
+		Kettle boiler=new Kettle();
+		boiler.boil();
+		Cup cup=new Cup();
+		cup.makeTea(passion, boiler.getWater());
 	}
 
 }
---
+++ src/_99_extra/PartyDancer.java
@@ -0,0 +1,11 @@
+package _99_extra;
+
+public class PartyDancer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PixelParty party= new PixelParty();
+		party.drawRectangle(40, 40, 20, 20);
+	}
+
+}
